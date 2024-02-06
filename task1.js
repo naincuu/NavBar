715dd8c18ffc94f6269fb54d3ba90b0887c4ecce
naincuu <naincybai.bscsf21@iba-suk.edu.pk>
2024-02-06 13:56:31 +0500

    add html file

---
+++ task1.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var navbar = document.querySelector(".ul");
+
+    navbar.addEventListener("click", function() {
+        navbar.style.backgroundColor = "white";
+    });
+});
